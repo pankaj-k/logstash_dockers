96b8664424962ea38ff2e2a0e7d4d81b048d6ec9
Pankaj K <pankaj.160907@gmail.com>
2025-03-13 07:49:29 +1100

    Checking scaling

--- terraform/lb_target_group.tf
+++ terraform/lb_target_group.tf
@@ -20,7 +20,7 @@ resource "aws_lb_target_group" "aws_load_balancer_target_group" {
   }
 }
 
-# Task is stopping
+# Task is stopping ! You see a set of 3 failed tasks. Then 3 tasks running. Why first 3 failed in ECS sservice?
 # Task failed ELB health checks in 
 # (target-group arn:aws:elasticloadbalancing:us-east-1:875792031575:targetgroup/aws-lb-target-group/2b48cbeac328fac3)
 # This usually happens when the ECS tasks take longer to become healthy than the ELB health check timeout allows.
