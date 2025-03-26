e9d5314212de421d0aec6d635f23b590a16207bf
Pankaj K <pankaj.160907@gmail.com>
2025-03-26 22:33:58 +1100

    Tried but failed to make eventbridge work.

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,13 +1,17 @@
+# TODO: Fix this. Somehow if you comment out 11 and put in 12 you get 26 emails. Always. 
+# The idea was to make sure that when a task is created or destroyed I get an email. 
+# Seems too much to ask. 
 resource "aws_cloudwatch_event_rule" "ecs_task_events" {
-  name        = "ecs-task-events"
-  description = "Trigger when an ECS task is started or stopped"
+  name        = "ecs-task-started"
+  description = "Trigger when an ECS task is started."
   event_pattern = jsonencode({
     source      = ["aws.ecs"]
     detail-type = ["ECS Task State Change"]
     detail = {
       clusterArn    = ["${aws_ecs_cluster.logstash_cluster.arn}"]
-      lastStatus    = ["RUNNING", "STOPPED"] # Ignore other states like PENDING, etc.
-      desiredStatus = ["RUNNING", "STOPPED"]
+      lastStatus    = ["STOPPED"] # Ignore other states like PENDING, etc.
+      stoppedReason = ["Essential container in task exited"]
+      #desiredStatus = ["RUNNING", "STOPPED"]
       # RUNNING + desiredStatus RUNNING : Only triggers when a task actually starts running.
       # STOPPED + desiredStatus STOPPED : Triggers when a task is truly stopped (not just transitioning).
     }
